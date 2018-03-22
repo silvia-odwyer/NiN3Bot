c36647c29512c97947011a53ee4764d363a8d7d1
silvia-odwyer <silviaodwyer119@yahoo.ie>
2018-03-22 15:55:28 +0000

    Now has a command to generate SVG patterns based on a string, thanks to an amazing Go library, but I still need to find a way to embed the SVGs.

--- discordBot.go
+++ discordBot.go
@@ -8,7 +8,9 @@ import (
 	"math/rand"
 	"strconv"
 	"time"
-	discord "github.com/bwmarrin/discordgo" 
+	"strings"
+	geopattern "github.com/pravj/geopattern"
+	discord "github.com/bwmarrin/discordgo"
 )
 
 func main(){
@@ -19,6 +21,7 @@ func main(){
 		return
 	}
 	session.AddHandler(newMessage)
+	fmt.Println("NiN3Bot has connected :)")
 	defer session.Close()
 
 	if err = session.Open(); err != nil {
@@ -33,37 +36,71 @@ func main(){
 }
 
 func newMessage(s *discord.Session, m *discord.MessageCreate){
+	println := fmt.Println
 	var outputMessage = ""
+	messageContent := m.Content
 	if m.Author.Bot  {
 		return
 	}
 
-	if m.Content == "9predict"{
-		var randomNumber = rand.Intn(5)
-		var a [5]string 
-		a[0] = "Tomorrow will be your lucky day."
-		a[1] = "Looks like good luck is on the way!"
-		a[2] = "Hmm, sorry, but I don't seem to be in a great mood for making predictions right now. :/"
-		a[3] =  "Watch out for a clue tomorrow."
-		a[4] = "You should keep an eye out for something cool tomorrow."
-	
-		var randomPrediction = a[randomNumber]
-		var stringOfNumber = strconv.Itoa(randomNumber)
-		var finalPrediction = "Your lucky number tomorrow is " + stringOfNumber
-		finalPrediction = randomPrediction + "\n" + finalPrediction
-		fmt.Println(finalPrediction)
-		outputMessage = finalPrediction
-	}
-	if m.Content == "9sayhi"{
-		outputMessage = "Hello o/"
-	}
-	if m.Content == "9repeat"{
-		outputMessage = m.Content
-	}
+	switch {
+	case strings.HasPrefix(messageContent, "9img"):
+		// Doesn't send the image yet, it generates the SVG but I need to work on embedding the SVG somehow :thinking:
+		alphabet := [26]string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+		
+		var randomPhrase = ""
+		for i := 0; i < 100; i++ {
+			randomNumber := rand.Intn(26)
+			println(randomNumber)
+			randomLetter := alphabet[randomNumber]
+			randomPhrase += randomLetter 
+		}
+		fmt.Println(randomPhrase)
+		outputMessage = "Generating image based on string: " + randomPhrase
+		args := map[string]string{"generator": "squares", "phrase" : randomPhrase}
+		generatedPattern := geopattern.Generate(args)
+		println(generatedPattern)
 
-	if m.Content == "9time"{
-		outputMessage = time.Now()
-	}
+	case messageContent == "9art":
+		outputMessage = "Ⓓ"
+
+
+	case strings.HasPrefix(messageContent, "9predict"):
+			var randomNumber = rand.Intn(5)
+			var a [5]string 
+			a[0] = "Tomorrow will be your lucky day."
+			a[1] = "Looks like good luck is on the way!"
+			a[2] = "Hmm, sorry, but I don't seem to be in a great mood for making predictions right now. :/"
+			a[3] =  "Watch out for a clue tomorrow."
+			a[4] = "You should keep an eye out for something cool tomorrow."
+		
+			var randomPrediction = a[randomNumber]
+			var stringOfNumber = strconv.Itoa(randomNumber)
+			var finalPrediction = "Your lucky number tomorrow is " + stringOfNumber
+			finalPrediction = randomPrediction + "\n" + finalPrediction
+			println(finalPrediction)
+			outputMessage = finalPrediction
+		
+	case strings.HasPrefix(messageContent, "9sayhi"):
+			outputMessage = "Hello o/"
+		
+	case strings.HasPrefix(messageContent, "9repeat"):
+			outputMessage = m.Content
+	
+	case strings.HasPrefix(messageContent, "9time"):
+			var currentTime = time.Now()
+	
+			// t, _ := time.Parse(time.UnixDate, outputMessage)
+			// println(t)
+	
+			// Yeah, I've got two ways of formatting the time, still have to decide which I prefer.
+			var formattedTime = currentTime.Format(time.RFC3339)
+			println(formattedTime)
+			//outputMessage = formattedTime
+			
+			formattedTime2 := currentTime.Format("3:04 PM, Monday Jan 2 2006 MST")
+			outputMessage = formattedTime2
+	} 
 	s.ChannelMessageSend(m.ChannelID, outputMessage)
 
 }
