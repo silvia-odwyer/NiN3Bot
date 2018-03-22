b2152341e664c91885f9d6b877bbce4d9e53a0a1
silvia-odwyer <silviaodwyer119@yahoo.ie>
2018-03-22 14:07:34 +0000

    NiN3 Bot now takes commands from the user, with a prefix of 9 for each command.

--- discordBot.go
+++ discordBot.go
@@ -6,11 +6,13 @@ import (
 	"os/signal"
 	"syscall"
 	"math/rand"
+	"strconv"
+	"time"
 	discord "github.com/bwmarrin/discordgo" 
 )
 
 func main(){
-	session, err := discord.New("Bot NDI2MTAwNjQzODMzMzE1MzI4.DZRXcg.0N9oJ7YIlOJIoZCHAmq2YHVVpuk")
+	session, err := discord.New("Bot TOKEN")
 		
 	if err != nil {
 		fmt.Println(err)
@@ -31,20 +33,37 @@ func main(){
 }
 
 func newMessage(s *discord.Session, m *discord.MessageCreate){
-
+	var outputMessage = ""
 	if m.Author.Bot  {
 		return
 	}
 
-	var randomNumber = rand.Intn(4)
-	var a [4]string 
-	a[0] = "Tomorrow will be your lucky day."
-	a[1] = "3 will be your lucky number."
-	a[2] = "Hmm, I don't seem to be in a great mood for making predictions right now."
-	a[3] =  "Watch out for a clue tomorrow."
+	if m.Content == "9predict"{
+		var randomNumber = rand.Intn(5)
+		var a [5]string 
+		a[0] = "Tomorrow will be your lucky day."
+		a[1] = "Looks like good luck is on the way!"
+		a[2] = "Hmm, sorry, but I don't seem to be in a great mood for making predictions right now. :/"
+		a[3] =  "Watch out for a clue tomorrow."
+		a[4] = "You should keep an eye out for something cool tomorrow."
+	
+		var randomPrediction = a[randomNumber]
+		var stringOfNumber = strconv.Itoa(randomNumber)
+		var finalPrediction = "Your lucky number tomorrow is " + stringOfNumber
+		finalPrediction = randomPrediction + "\n" + finalPrediction
+		fmt.Println(finalPrediction)
+		outputMessage = finalPrediction
+	}
+	if m.Content == "9sayhi"{
+		outputMessage = "Hello o/"
+	}
+	if m.Content == "9repeat"{
+		outputMessage = m.Content
+	}
 
-	var randomPrediction = a[randomNumber]
-
-	s.ChannelMessageSend(m.ChannelID, randomPrediction)
+	if m.Content == "9time"{
+		outputMessage = time.Now()
+	}
+	s.ChannelMessageSend(m.ChannelID, outputMessage)
 
 }
